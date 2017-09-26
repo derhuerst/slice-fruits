a952723c9fbeaaa520c9fb46cb47b6a8e486355b
Jannis R <mail@jannisr.de>
2017-09-26 22:07:57 +0200

    init from strategy-game

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const document = require('global/document')
+const window = require('global/window')
+const raf = require('raf')
+
+const getCanvas = require('./lib/get-canvas')
+const renderGame = require('./lib/render')
+
+const state = {
+}
+
+const actions = {
+}
+
+const {canvas, ctx} = getCanvas()
+canvas.style.position = 'absolute'
+canvas.style.left = '0'
+canvas.style.top = '0'
+canvas.style.width = '100%'
+canvas.style.height = '100%'
+document.body.appendChild(canvas)
+
+const render = () => {
+	const s = performance.now()
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+	renderGame(ctx, state, actions)
+
+	const t = performance.now() - s
+	if (process.env.NODE_ENV === 'dev' && (t|0) >= 5) {
+		console.warn('rendering took', t|0, 'ms')
+	}
+
+	// raf(render) // todo
+	setTimeout(render, 100)
+}
+render()
