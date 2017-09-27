7695b9b216ab8e405c5616ed320c709508470f0c
Jannis R <mail@jannisr.de>
2017-09-27 16:26:27 +0200

    basic cut trail

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 const document = require('global/document')
 const window = require('global/window')
 const raf = require('raf')
+// const debounce = require('lodash/debounce')
 
 const getCanvas = require('./lib/get-canvas')
 const createTick = require('./lib/tick')
 const renderGame = require('./lib/render')
+const renderCutTrail = require('./lib/cut-trail')
 
 const state = {
 	endOfLastBurst: 0,
@@ -24,6 +26,7 @@ canvas.style.width = '100%'
 canvas.style.height = '100%'
 document.body.appendChild(canvas)
 
+renderCutTrail(state, canvas, ctx)
 const tick = createTick(state)
 setInterval(tick, 10)
 
